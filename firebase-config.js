1c696c216a5014acd9d209a3174ca0eff4833cab
scrutinyacademy <scrutinyacademy@gmail.com>
2026-09-18 02:02:33 +0530

    Add Firebase web configuration for Board Booster app

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDxBIBifpRdB4iVGEqX_mlJrHILRn7g5e4",
+  authDomain: "scrutiny-academy.firebaseapp.com",
+  projectId: "scrutiny-academy",
+  storageBucket: "scrutiny-academy.firebasestorage.app",
+  messagingSenderId: "49190910579",
+  appId: "1:49190910579:web:f6962fa73843a4cac70806",
+  measurementId: "G-XV08J17YB5"
+};
